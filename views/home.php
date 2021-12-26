acf9c5e5369b2c229eb75b22e57dd02b36601dc0
Ramon Gallo <ramong1145@gmail.com>
2021-12-25 18:12:22 -0600

    ADDED layout and home view

---
+++ views/home.php
@@ -0,0 +1,4 @@
+<div class="col-8">
+    <h1> Home Page </h1>
+    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam tempora earum, numquam aut est at quod optio, impedit suscipit amet inventore laudantium hic deserunt unde quaerat voluptatum pariatur in commodi. </p>
+</div>
